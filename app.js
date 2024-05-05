8f3504daba49fe4a168929c9edda29cca15282a3
shubui10 <108941866+shubui10@users.noreply.github.com>
2024-05-05 13:34:20 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ import { Hono } from "https://deno.land/x/hono@v3.7.4/mod.ts";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hi there!"));
+app.get("/", (c) => c.text("Hello Web Software Development!"));
 
 export default app;
